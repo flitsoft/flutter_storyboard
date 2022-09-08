5f5949dbd5fd957ad352e9e45d926034b0f5b03a
Samuel <tolotrasam@gmail.com>
2022-09-08 01:43:24 -0400

    Fixed hanging on feature branch data store not found

--- lib/src/storyboard_updater.dart
+++ lib/src/storyboard_updater.dart
@@ -4,13 +4,19 @@ import 'package:flutter_storyboard/src/utils/runner_messenger.dart';
 
 class StoryboardUpdater {
   Future<void> main() async {
+    print("StoryboardUpdater.main");
     final baseBranch =
         const String.fromEnvironment(STORYBOARD_BASE_BRANCH_NAME);
     final featureBranch =
         const String.fromEnvironment(STORYBOARD_FEATURE_BRANCH_NAME);
     final repo = StoryboardRepository();
     final data = await repo.readGraphFlow(featureBranch);
-    if (data == null) return;
+    if (data == null) {
+      print("Feature branch not found storyboard data not found. Exiting.");
+      RunnerMessenger.sendMessage(CLOSE_STORYBOARD, "");
+      return;
+    }
+    ;
     final newData = data.copyWith(branchName: baseBranch);
     await repo.saveGraphFlow(newData);
     RunnerMessenger.sendMessage(BASE_BRANCH_UPDATED, "");
